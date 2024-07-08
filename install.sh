6061f7c94b10b7de33b40cbdbabd620d98f26c1e
Jake Degiovanni <degiojake@gmail.com>
2024-07-08 09:28:09 +0100

    BT-175: adding gotemplate to buf.gen.yaml

--- install.sh
+++ install.sh
@@ -2,5 +2,6 @@
 
 go install github.com/sqlc-dev/sqlc/cmd/sqlc@latest
 go install github.com/bufbuild/buf/cmd/buf@latest
+go install moul.io/protoc-gen-gotemplate@latest
 
 go build -o "$(go env GOPATH)"/bin/es-sqlc-grpc .
