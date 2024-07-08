b12d421c0986333b52d93b6058ebcc7bb9f854de
Jake Degiovanni <degiojake@gmail.com>
2024-07-08 12:02:41 +0100

    BT-175: using es gotempalte tool

--- install.sh
+++ install.sh
@@ -2,6 +2,5 @@
 
 go install github.com/sqlc-dev/sqlc/cmd/sqlc@latest
 go install github.com/bufbuild/buf/cmd/buf@latest
-go install moul.io/protoc-gen-gotemplate@latest
 
 go build -o "$(go env GOPATH)"/bin/es-sqlc-grpc .
